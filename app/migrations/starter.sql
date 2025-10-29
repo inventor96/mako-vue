941d4e1aabddf80eeb510599b052139d9b512048
Caleb Hornbeck <inventor96@gmail.com>
2025-10-28 21:47:58 -0600

    Fix starter sql

--- app/migrations/starter.sql
+++ app/migrations/starter.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE boxbeacon CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'boxbeacon'@'%' IDENTIFIED BY 'localtest';
-GRANT ALL PRIVILEGES ON boxbeacon.* TO 'boxbeacon'@'%';
-USE boxbeacon;
+CREATE DATABASE makovue CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER 'makovue'@'%' IDENTIFIED BY 'localtest';
+GRANT ALL PRIVILEGES ON makovue.* TO 'makovue'@'%';
+USE makovue;
 CREATE TABLE `mako_migrations`
 (
 	`batch` int(10) unsigned NOT NULL,
