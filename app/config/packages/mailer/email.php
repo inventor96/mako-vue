8bd1685cb5346a5bf279617d2d236946f6aa9c3c
Caleb Hornbeck <inventor96@gmail.com>
2026-01-29 04:13:40 -0700

    Updated core email config to match package update

--- app/config/packages/mailer/email.php
+++ app/config/packages/mailer/email.php
@@ -1,11 +1,55 @@
 <?php
 return [
+	/**
+	 * The name of the sender.
+	 */
 	'from_name' => 'MakoVue',
+
+	/**
+	 * The email address of the sender.
+	 */
 	'from_email' => 'noreply@makovue.test',
+
+	/**
+	 * The email adapter class to use.
+	 * Must implement `inventor96\MakoMailer\interfaces\EmailSenderInterface`.
+	 */
+	'adapter' => inventor96\MakoMailer\adapters\PHPMailerAdapter::class,
+
+	/**
+	 * Whether to use SMTP for sending emails.
+	 * Set to `false` to use the mail() function.
+	 */
+	'use_smtp' => true,
+
+	/**
+	 * SMTP server host.
+	 */
 	'host' => '',
+
+	/**
+	 * SMTP server port.
+	 */
 	'port' => 465,
-	//'encryption' => PHPMailer::ENCRYPTION_SMTPS,
+
+	/**
+	 * Encryption method to use.
+	 * Set to empty string to disable encryption.
+	 */
+	//'encryption' => PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS,
+
+	/**
+	 * Whether to use SMTP authentication.
+	 */
 	//'auth' => true,
-	//'username' => 'noreply@makovue.test',
-	//'password' => '',
+
+	/**
+	 * SMTP username.
+	 */
+	//'username' => 'noreply@example.com',
+
+	/**
+	 * SMTP password.
+	 */
+	//'password' => 'MySecurePassword123!',
 ];
