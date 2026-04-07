946b072a35b9b13170266aefd288517f950005da
Caleb Hornbeck <inventor96@gmail.com>
2026-04-07 03:32:00 -0600

    Use `npm ci` opportunistically

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -4,8 +4,13 @@ set -e
 # If we're running npm run dev, double check that npm dependencies are installed
 if [ "$1" = "npm" ] && [ "$2" = "run" ] && [ "$3" = "dev" ]; then
   if [ ! -d "node_modules" ]; then
-    echo "Node modules not found. Installing..."
-    npm install
+    if [ -f "package-lock.json" ]; then
+      echo "Node modules not found. Installing with npm ci..."
+      npm ci
+    else
+      echo "Node modules not found. Installing with npm install..."
+      npm install
+    fi
   fi
 fi
 
