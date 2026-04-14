3b25ef78bd4c3138ed0f1384f660cdbd6df313ea
Caleb Hornbeck <inventor96@users.noreply.github.com>
2026-04-13 23:49:45 -0600

    Fix database credentials in docker config

--- app/config/docker/database.php
+++ app/config/docker/database.php
@@ -6,8 +6,8 @@ return (function() {
 	$database = require(__DIR__.'/../database.php');
 	$default = $database['default'];
 	$database['configurations'][$default]['dsn'] = 'mysql:dbname=' . env('MARIADB_DATABASE') . ';host=db;port=3306';
-	$database['configurations'][$default]['username'] = env('MARIADB_MIGRATION_USER');
-	$database['configurations'][$default]['password'] = env('MARIADB_MIGRATION_PASSWORD');
+	$database['configurations'][$default]['username'] = env('MARIADB_USER');
+	$database['configurations'][$default]['password'] = env('MARIADB_PASSWORD');
 	$database['configurations'][$default]['log_queries'] = true;
 	return $database;
 })();
